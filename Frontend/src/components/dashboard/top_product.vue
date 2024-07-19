b5dd064840d2a379ff488ababc27901b2a75da24
Reza Ali Nirwansyah <rezaalinirwansyah@gmail.com>
2024-07-19 14:56:04 +0700

    Update top_product.vue

--- Frontend/src/components/dashboard/top_product.vue
+++ Frontend/src/components/dashboard/top_product.vue
@@ -1,3 +1,42 @@
 <template>
     <h2><b>Top Product</b></h2>
+
+    <ul class="list-group list-group-flush">
+        <li class="list-group-item d-flex">
+            <div>
+                ikan tongkaol
+            </div>
+
+            <div class="ms-auto">
+                <p>ikan</p>
+                sales
+            </div>
+        </li>
+    </ul>
 </template>
+
+<script>
+export default {
+    name : 'top_product',
+    data(){
+        return {
+            top_product : [],
+        }
+    },
+    created(){
+
+    },
+    methods:{
+        fetchData(){
+            fetch('/data/top_product.json')
+            .then(response => response.json())
+            .then(data => {
+                this.top_product = data
+            })
+            .catch(error => {
+                console.error('Error fetching data:', error)
+            })
+        }
+    }
+}
+</script>
